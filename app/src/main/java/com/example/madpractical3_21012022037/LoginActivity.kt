f0d5058849c8f9b672f2368f3f5724c128657630
sikandargit <sikandardamor21@gnu.ac.in>
2022-08-24 15:22:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/madpractical3_21012022037/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.example.madpractical3_21012022037
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.WindowCompat
+import com.example.madpractical3_21012022037.databinding.ActivityLoginBinding
+import com.example.madpractical3_21012022037.databinding.ActivityMainBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+    }
+}
