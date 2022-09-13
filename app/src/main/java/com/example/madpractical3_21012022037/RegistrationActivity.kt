82cbf89141f52e8c4e0e4f00a7fde5f02a0033bd
Harsh <harsh.sumanpatel2003@gmail.com>
2022-09-13 23:56:29 +0530

    practical 3

--- app/src/main/java/com/example/madpractical3_21012022037/RegistrationActivity.kt
+++ app/src/main/java/com/example/madpractical3_21012022037/RegistrationActivity.kt
@@ -1,20 +1,35 @@
 package com.example.madpractical3_21012022037
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.view.WindowCompat
 import com.example.madpractical3_21012022037.databinding.ActivityMainBinding
+import com.example.madpractical3_21012022037.databinding.ActivityRegistrationBinding
 
 class RegistrationActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-
+    private lateinit var binding: ActivityRegistrationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityRegistrationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
+        binding.bottomNavigationView.selectedItemId = R.id.bottom_reg_registration
+        binding.bottomNavigationView.setOnItemSelectedListener{it1->
+            when(it1.itemId)
+            {
+                R.id.bottom_reg_registration->
+                {
+                    Intent(this,LoginActivity::class.java).also {startActivity(it)}
+                }
+            }
+            return@setOnItemSelectedListener true
+        }
+
+        binding.tvLogin.setOnClickListener {
+            Intent(this,LoginActivity::class.java).apply {
+                startActivity(this)
+            }
+        }
     }
 }
